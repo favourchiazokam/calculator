48a3c0b7a807f10e3e5a2706e9eec98fc3298bce
favour <favourthegracious@gmail.com>
2020-03-26 15:07:23 +0100

    calculator evaluation

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -1,5 +1,5 @@
 import "./style.css";
-import React from "react";
+import React, { useState } from "react";
 
 function Button({ title, action, actionBtn, long }) {
   return (
@@ -13,7 +13,7 @@ function Button({ title, action, actionBtn, long }) {
 }
 
 function MyApp() {
-  let value = "";
+  const [value, setValue] = useState("");
 
   return (
     <div className="body">
@@ -21,33 +21,43 @@ function MyApp() {
         <div className="screen">{value}</div>
         <div className="buttons">
           <div className="group">
-            <Button title="C" />
-            <Button title="(" />
-            <Button title=")" />
-            <Button title="÷" actionBtn />
+            <Button title="C" action={() => setValue("")} />
+            <Button title="(" action={() => setValue(value + "(")} />
+            <Button title=")" action={() => setValue(value + ")")} />
+            <Button title="÷" actionBtn action={() => setValue(value + "/")} />
           </div>
           <div className="group">
-            <Button title="7" />
-            <Button title="8" />
-            <Button title="9" />
-            <Button title="X" actionBtn />
+            <Button title="7" action={() => setValue(value + "7")} />
+            <Button title="8" action={() => setValue(value + "8")} />
+            <Button title="9" action={() => setValue(value + "9")} />
+            <Button title="X" actionBtn action={() => setValue(value + "*")} />
           </div>
           <div className="group">
-            <Button title="4" />
-            <Button title="5" />
-            <Button title="6" />
-            <Button title="-" actionBtn />
+            <Button title="4" action={() => setValue(value + "4")} />
+            <Button title="5" action={() => setValue(value + "5")} />
+            <Button title="6" action={() => setValue(value + "6")} />
+            <Button title="-" actionBtn action={() => setValue(value + "-")} />
           </div>
           <div className="group">
-            <Button title="1" />
-            <Button title="2" />
-            <Button title="3" />
-            <Button title="+" actionBtn />
+            <Button title="1" action={() => setValue(value + "1")} />
+            <Button title="2" action={() => setValue(value + "2")} />
+            <Button title="3" action={() => setValue(value + "3")} />
+            <Button title="+" actionBtn action={() => setValue(value + "+")} />
           </div>
           <div className="group">
-            <Button title="0" long />
-            <Button title="." />
-            <Button title="=" actionBtn />
+            <Button title="0" long action={() => setValue(value + "0")} />
+            <Button title="." action={() => setValue(value + ".")} />
+            <Button
+              title="="
+              actionBtn
+              action={() => {
+                try {
+                  setValue(eval(value));
+                } catch (error) {
+                  alert(error);
+                }
+              }}
+            />
           </div>
         </div>
       </div>
